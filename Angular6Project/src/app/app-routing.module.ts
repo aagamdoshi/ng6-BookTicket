67034be01e6ddbec40e4c4d64954e6d5d359d73c
Aagam Doshi <aagamdoshi30794@gmail.com>
2019-06-05 21:43:12 +0530

    Basic Architecture of ng6

---
+++ Angular6Project/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BookMovieComponent } from './book-movie/book-movie.component';
+import { ShowBookingsComponent } from './show-bookings/show-bookings.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'bookMovie', component: BookMovieComponent },
+  { path: 'bookings', component: ShowBookingsComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Angular6Project/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookMovieComponent } from './book-movie/book-movie.component';
+import { ShowBookingsComponent } from './show-bookings/show-bookings.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookMovieComponent,
+    ShowBookingsComponent,
+    HomeComponent
+  ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
