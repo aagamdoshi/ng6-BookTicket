b8ee6be146867e53e3114be0d7deb09aa7cb9fe8
Aagam Doshi <aagamdoshi30794@gmail.com>
2019-06-09 02:22:31 +0530

    HTML CSS changes for HOME and Bookings page , Data filing

--- Angular6Project/src/app/book-movie/book-movie.component.ts
+++ Angular6Project/src/app/book-movie/book-movie.component.ts
@@ -7,20 +7,58 @@ import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms'
   styleUrls: ['./book-movie.component.scss']
 })
 export class BookMovieComponent implements OnInit {
-
-  name = new FormControl('');
-
+  make: any;
+  year: any;
+  model: any;
+  timings: string[];
+  persons: string[];
   constructor(private fb: FormBuilder) { }
 
   movieDetailsForm: FormGroup;
+  movieNames: any[] = [{ id: 1, name: 'Shawshank Redemption' },
+  { id: 2, name: 'Batman vs Superman' },
+  { id: 3, name: 'Avengers Endgame' },
+  { id: 4, name: 'Harry Potter' },
+  { id: 5, name: 'Aquaman' }];
+  listMovieDetails: any;
 
   ngOnInit() {
-        /* Initiate the form structure */
-        this.movieDetailsForm = this.fb.group({
-          movieName: [''],
-          timings: [''],
-          persons: [''],
-        });
+    this.movieDetailsForm = this.fb.group({
+      movieName: [''],
+      timings: [''],
+      persons: [''],
+    });
   }
 
+  onMovieChange(data) {
+    switch (this.movieDetailsForm.value.movieName) {
+      case 'Shawshank Redemption': {
+        this.timings = ['10:00', '11:00', '13:00', '17:00', '20:00'];
+      }
+        break;
+      case 'Batman vs Superman': {
+        this.timings = ['10:00', '11:00', '13:00'];
+      }
+        break;
+      case 'Avengers Endgame': {
+        this.timings = ['10:00', '11:00', '13:00', '15:00'];
+      }
+        break;
+      case 'Harry Potter': {
+        this.timings = ['07:00', '12:00', '14:00', '18:00'];
+      }
+        break;
+      case 'Aquaman': {
+        this.timings = ['12:00', '14:00'];
+      }
+        break;
+      default: {
+        this.timings = ['10:00', '11:00', '13:00', '17:00', '20:00'];
+      }
+    }
+  }
+
+  onBookTickets(): void {
+    console.log(this.movieDetailsForm.value);
+  }
 }
